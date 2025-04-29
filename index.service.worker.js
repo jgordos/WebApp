dadd7aade652088d928b5c1cda7f71b22091deff
John A. Gordos, III <jgordos@yahoo.com>
2025-04-29 15:41:07 -0400

    Icon replacement
    Also no splash screen

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745889828|2902618434';
+const CACHE_VERSION = '1745953019|1405192992';
 /** @type {string} */
 const CACHE_PREFIX = 'GodotIOSScreenSt-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
