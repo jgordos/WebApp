90408aeb485a331018125d2e34dca719e83211d2
John A. Gordos, III <jgordos@yahoo.com>
2025-05-01 13:50:29 -0400

    Removed the EXPERIMENTAL VIRTUAL KEYBOARD because it sucked
    Removed the EXPERIMENTAL VIRTUAL KEYBOARD because it sucked

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746114729|663594405';
+const CACHE_VERSION = '1746121236|115632021';
 /** @type {string} */
 const CACHE_PREFIX = 'John Gordos' Tex-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
