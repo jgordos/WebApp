efe31234dd35a25cac3251084539ef8647b024d7
John A. Gordos, III <jgordos@yahoo.com>
2025-05-01 17:32:02 -0400

    Version 2025.05.01.05
    Added table calculation
    Redid title screen

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746124044|1301798282';
+const CACHE_VERSION = '1746134888|17461827';
 /** @type {string} */
 const CACHE_PREFIX = 'John Gordos' Tex-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
